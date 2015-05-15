9c8cc4e56c3e1c72276bb6aa66ac7a946c793c28
Preety Pednekar <preety@Preetys-MacBook-Pro.local>
2015-05-15 18:09:00 +0900

    Initial Commit

---
+++ CustomCalender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCalender
+//
+//  Created by Preety Pednekar on 15/05/15.
+//  Copyright (c) 2015 Preety Pednekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
